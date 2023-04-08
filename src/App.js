153d646d2ce9648f1ac3235ae89da68d76f2c10e
Alexisvip739 <39203602@uaz.edu.mx>
2023-04-08 12:52:56 -0600

    Se agrego todo el proyecto

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import './App.css'
+import Header from './components/header/Header.jsx'
+import Home from './pages/home/Home'
+import MovieDetail from './pages/movieDetail/movieDetail'
+import MovieList from './components/movieList/movieList'
+
+function App() {
+
+  return (
+    <div className="App">
+        <Router>
+          <Header />
+            <Routes>
+                <Route index element={<Home />}></Route>
+                <Route path="movie/:id" element={<MovieDetail />}></Route>
+                <Route path="movies/:type" element={<MovieList />}></Route>
+                <Route path="/*" element={<h1>Error Page</h1>}></Route>
+            </Routes>
+        </Router>
+    </div>
+  );
+}
+
+export default App
