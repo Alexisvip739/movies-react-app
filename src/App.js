c6906cfc26a518d6abc5ae73454db1f769aea906
kueski_1000aus <ultrerass01@hotmail.com>
2023-07-17 09:13:17 -0600

    MovieDetail responsive

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Router>
           <Header />
             <Routes>
-                <Route index element={<Home />}></Route>
+                <Route path="/movies-react-app" element={<Home />}></Route>
                 <Route path="movie/:id" element={<MovieDetail />}></Route>
                 <Route path="movies/:type" element={<MovieList />}></Route>
                 <Route path="/*" element={<h1>Error Page</h1>}></Route>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom'
 function Header() {
   return (
     <div className="header">
-    <div className="headerLeft">
-        <Link to="/"><img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" /></Link>
-        <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
-        <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
-        <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
-    </div>
-</div>
+      <div className="headerLeft">
+          <Link to="/movies-react-app"><img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" /></Link>
+          <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+          <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top</span></Link>
+          <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+      </div>
+  </div>
   )
 }
 
